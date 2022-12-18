e4a2d0a381cc38e411689a8d21cf927440015fc7
Thiago Shibanuma <thiago.tshibanuma@gmail.com>
2022-12-18 17:57:04 -0300

    criação da função que retorna o nome do estudante e sua atividades

--- index.js
+++ index.js
@@ -1,31 +1,56 @@
-searchStudentsById(1)
-
 // Part 1
 
-function searchStudentsById(id){
-    fetch('https://apigenerator.dronahq.com/api/g4C15xPP/students/' + id)
-    .then(function (response) {
-        return response.json();
-    }).then(function (data) {
-        console.log(compareStudentId(data,id))
-    });
+returnStudentName(1)
+    .then(function (studentName) {
+        console.log(studentName)
+    })
+    .then(function () {
+        returnTaskId(2)
+            .then(function (array) {
+                return returnTaskName(array);
+            })
+            .then(function (names) {
+                console.log(names)
+            })
+    })
+
+async function returnStudentName(id) {
+    let student = await fetch('https://apigenerator.dronahq.com/api/g4C15xPP/students/' + id);
+    student = await student.json();
+    let name = student.Name;
+    return name
 }
 
-function returnTaskId(studentId){
-    fetch('https://apigenerator.dronahq.com/api/5Bba_f-L/grades')
-    .then(function (response) {
-        return response.json();
-    }).then(function (data) {
-        console.log(compareStudentId(data, studentId));
-    });
-}
-
-function compareStudentId(json, studentId) {
-    let array = []
-    for(let index = 0; index < json.length; index++){
-        if (parseInt(json[index].studentId) === studentId){
-            array.push(json[index].taskId)
-        } 
+async function returnTaskId(id) {
+    let arrayTask = [];
+    let task = fetch('https://apigenerator.dronahq.com/api/5Bba_f-L/grades');
+    task = await (await task).json();
+    for (let index = 0; index < task.length; index++) {
+        if (id === parseInt(task[index].studentId)) {
+            let studentTask = {
+                "taskId": task[index].taskId,
+                "taskGrade": task[index].number
+            }
+            arrayTask.push(studentTask)
+        }
     }
-    return array
+    return arrayTask
+}
+
+async function returnTaskName(array) {
+    let tasksArray = [];
+    let tasks = fetch('https://apigenerator.dronahq.com/api/75U0yEKU/tasks');
+    tasks = await (await tasks).json();
+    array.forEach(x => {
+        for (let index = 0; index < tasks.length; index++) {
+            if (parseInt(x.taskId) === parseInt(tasks[index].id)) {
+                let task = {
+                    "taskName": tasks[index].title,
+                    "taskGrade": x.taskGrade
+                }
+                tasksArray.push(task)
+            }
+        }
+    })
+    return tasksArray
 }
